201a09ad3c436ecf25760d0e6dc9a2e54a05c3c0
Mariana Martins <marih_ms@hotmail.com>
2024-03-04 18:00:00 -0300

    primeira versao com localStorage

--- script.js
+++ script.js
@@ -2,6 +2,58 @@ let botaoNovaTarefa = document.getElementById("botao-nova-tarefa");
 let listaTarefas = document.getElementById("lista-tarefas");
 let novaAtividade = document.getElementById("nova-atividade");
 
+function salvarTarefasLocalStorage() {
+    let li = listaTarefas.children;
+    let tarefas = [];
+
+    for (let i = 0; i < li.length; i++) {
+        let tarefa = {
+            nomeDaTarefa: li[i].innerText,
+            tarefaConcluida: li[i].querySelector('input[type="checkbox"]').checked
+        };
+        tarefas.push(tarefa);
+    }
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+}
+
+function carregarTarefasLocalStorage() {
+    let tarefas = JSON.parse(localStorage.getItem("tarefas"));
+
+    if (tarefas) {
+        for (let i = 0; i < tarefas.length; i++) {
+            /*recria os elementos a partir do localStorage
+            recria a função da lixeira*/
+            let checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.checked = tarefas[i].tarefaConcluida;
+            let li = document.createElement("li");
+            let iconeLixeira = document.createElement("img");
+            iconeLixeira.src = "img/lixeira-x.png"
+            iconeLixeira.style.float = "right";
+            let span = document.createElement("span");
+
+            span.appendChild(document.createTextNode(tarefas[i].nomeDaTarefa));
+            li.appendChild(checkbox);
+            li.appendChild(span);
+            li.appendChild(iconeLixeira);
+
+            iconeLixeira.addEventListener("click", function() {
+                let confirmacao = confirm("A exclusão não pode ser revertida, tem certeza?");
+                if (confirmacao) {
+                    this.parentElement.remove();
+                    atualizarContador();
+                    salvarTarefasLocalStorage();
+                }
+            });
+
+            listaTarefas.appendChild(li);
+        }
+    }
+}
+
+window.onload = carregarTarefasLocalStorage;
+
 botaoNovaTarefa.addEventListener("click",  function() {
     if (novaAtividade.value !== "") {
         let checkbox = document.createElement("input");
@@ -21,7 +73,8 @@ botaoNovaTarefa.addEventListener("click",  function() {
             let confirmacao = confirm("A exclusão não pode ser revertida, tem certeza?");
             if (confirmacao) {
                 this.parentElement.remove()   
-                atualizarContador();     
+                atualizarContador();  
+                salvarTarefasLocalStorage();   
             }
         });
            
@@ -37,15 +90,12 @@ botaoNovaTarefa.addEventListener("click",  function() {
         novaAtividade.value = "";
         atualizarContador();
 
-        // localStorage.setItem("listaTarefas", listaTarefas.innerHTML);
-        // listaTarefas.innerHTML = localStorage.getItem("listaTarefas");
-
         iconeLixeira.className ="icone-lixeira";
         checkbox.className = "checkbox";
+
+        salvarTarefasLocalStorage();
     }
 });
-
-
 
 function atualizarContador() {
     let contador = document.getElementById("contador-texto");
